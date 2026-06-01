0ecda54756c66fd395ecab04be67823f3b81a2f4
MargaridaTeles <telesmaegarida@gmail.com>
2026-06-01 18:00:11 +0100

    Add new project TheyCameBefore

---
+++ TheyCameBefore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheyCameBefore
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
