fb1629e2e76f6d1193ee1a57080b9e0cff7152f3
MargaridaTeles <telesmaegarida@gmail.com>
2026-06-02 12:47:24 +0100

    Add new project JustLikeACollection

---
+++ JustLikeACollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JustLikeACollection
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
