3c85d805007274a45cbec824effc81e91b909483
MargaridaTeles <telesmaegarida@gmail.com>
2026-06-02 12:19:47 +0100

    Add new project MyGenericClass

---
+++ MyGenericClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGenericClass
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
